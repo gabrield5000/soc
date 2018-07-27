ab72b00cc6b6155a65400d198a3ffc85c6c99511
gabrield5000 <gabrield5000@gmail.com>
2018-07-28 00:20:58 +0300

    add product workflow

---
+++ api/models/order.js
@@ -0,0 +1,12 @@
+const mongoose = require('mongoose');
+
+const orderSchema = mongoose.Schema({
+    // _id: mongoose.Schema.Types.ObjectId,
+    // name: String,
+    // price: Number,
+    // date: { type: Date, default: Date.now }
+    product: { type: mongoose.Schema.Types.ObjectId, ref: 'Product', required: true },
+    quantity: {type: Number, default: 1 }
+});
+
+module.exports = mongoose.model('Order', orderSchema);
--- api/routes/products.js
+++ api/routes/products.js
@@ -5,10 +5,18 @@ const Product = require('../models/product');
 
 router.get('/', (req, res, next) => {
     Product.find()
+        .select( 'name price _id' )
         .exec()
         .then(docs => {
-            console.log(docs);
-            res.status(200).json(docs);
+            // console.log(docs);
+            const response = {
+                count: docs.length,
+                request: {
+                    type: 'GET'
+                },
+                products: docs
+            }
+            res.status(200).json(response);
         })
         .catch(err => {
             console.log(err);
@@ -28,15 +36,46 @@ router.post('/', (req, res, next) => {
     product
         .save()
         .then(result => {
-            console.log(result);
-            res.status(201).json({ massage: 'Handling Post product created to /product', createdProduct: result});
+            // console.log(result);
+            res.status(201).json({ massage: 'Created product successfully', 
+                                   createdProduct: {
+                                        name: result.name,
+                                        price: result.price,
+                                        request: {
+                                            type: 'POST'
+                                        }
+                                   }
+            });
         })
         .catch(err => {
             console.log(err);
             res.status(500).json({ error: err });
         });
-
    
+});
+
+router.get('/:id', (req, res, next) => {
+    const id = req.params.id;
+    Product.findById(id)
+        .select( 'name price _id' )
+        .exec()
+        .then( doc => {
+            console.log('get one entry from the DB', doc);
+            if (doc) {
+                res.status(200).json({
+                    product: doc,
+                    request: {
+                        type: "GET"
+                    }
+                });
+            } else {
+                res.status(404).json({ massage: "No valid entry found for provided ID" });
+            }
+        })
+        .catch(err => {
+            console.log(err);
+            res.status(500).json({ error: err });
+        });
 });
 
 router.patch('/:id', (req, res, next) => {
@@ -49,7 +88,12 @@ router.patch('/:id', (req, res, next) => {
         .exec()
         .then( result => {
             console.log(result);
-            res.status(200).json(result);
+            res.status(200).json({
+                massage: 'product updated',
+                request: {
+                    type: 'PATCH'
+                }
+            });
         })
         .catch(err => {
             console.log(err);
@@ -58,9 +102,25 @@ router.patch('/:id', (req, res, next) => {
   
 });
 
-router.get('/:id', (req, res, next) => {
-    const id = req.params.id;
-    res.status(200).json({ massage: ` your id ${id}`});
-});
+router.delete("/:id", (req, res, next) => {
+    const id = req.params.productId;
+    Product.remove({ _id: id })
+      .exec()
+      .then(result => {
+        res.status(200).json({
+            message: 'Product deleted',
+            request: {
+                type: 'POST',
+                body: { name: 'String', price: 'Number' }
+            }
+        });
+      })
+      .catch(err => {
+        console.log(err);
+        res.status(500).json({
+          error: err
+        });
+      });
+  });
 
 module.exports = router;
