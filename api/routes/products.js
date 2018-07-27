3e4aa624a47b871b12b6bf496b220136163fbe22
gabrield5000 <gabrield5000@gmail.com>
2018-07-27 22:11:42 +0300

    add model

--- api/routes/products.js
+++ api/routes/products.js
@@ -1,12 +1,61 @@
 const express = require('express');
 const router = express.Router();
+const mongoose = require('mongoose');
+const Product = require('../models/product');
 
 router.get('/', (req, res, next) => {
-    res.status(200).json({ massage: 'Handling GET requests to /product'});
+    Product.find()
+        .exec()
+        .then(docs => {
+            console.log(docs);
+            res.status(200).json(docs);
+        })
+        .catch(err => {
+            console.log(err);
+            res.status(500).json({
+                error: err
+            });
+        });
 });
 
 router.post('/', (req, res, next) => {
-    res.status(201).json({ massage: 'Handling Post product created to /product'});
+    console.log(req.body);
+    const product = new Product({
+        name: req.body.name,
+        price: req.body.price,
+    });
+
+    product
+        .save()
+        .then(result => {
+            console.log(result);
+            res.status(201).json({ massage: 'Handling Post product created to /product', createdProduct: result});
+        })
+        .catch(err => {
+            console.log(err);
+            res.status(500).json({ error: err });
+        });
+
+   
+});
+
+router.patch('/:id', (req, res, next) => {
+    const id = req.params.id;
+    const updateOps = {};
+    for (const ops of req.body) {
+        updateOps[ops.propName] = ops.value;
+    }
+    Product.update({ _id: id }, { $set: updateOps })
+        .exec()
+        .then( result => {
+            console.log(result);
+            res.status(200).json(result);
+        })
+        .catch(err => {
+            console.log(err);
+            res.status(500).json({ error: err });
+        });
+  
 });
 
 router.get('/:id', (req, res, next) => {
--- app.js
+++ app.js
@@ -1,12 +1,39 @@
 const express = require('express');
 const app = express();
 const morgan = require('morgan');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
 
 const orderRoutes = require('./api/routes/orders.js');
 const productRoutes = require('./api/routes/products.js');
 
+const options = {
+    socketTimeoutMS: 0,
+    keepAlive: true,
+    reconnectTries: 30,
+    useNewUrlParser: true
+}
+
+const dbUrl = 'mongodb://gabi:dcrhtk_1980@ds129541.mlab.com:29541/gabi';
+
+mongoose.connect(dbUrl, options).then(
+    () => { console.log('db conncted') },
+    err => {console.log(err)}
+);
 
 app.use(morgan('dev'));
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use((req, res, next) => {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers","Origin, X-Requested-With, Content-Type, Accept, Authorization");
+    if (req.method === 'OPTIONS') {
+        res.header('Access-Control-Allow-Methods', 'PUT, POST, PATCH, DELETE, GET');
+        return res.status(200).json({});
+    }
+    next();
+})
+
 app.use('/orders', orderRoutes);
 app.use('/products', productRoutes);
 
