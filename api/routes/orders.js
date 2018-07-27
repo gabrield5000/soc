13c5fcf105f28093771b4666544262ff7cebc16b
gabrield5000 <gabrield5000@gmail.com>
2018-07-27 14:21:43 +0300

    add error and routes

--- api/routes/orders.js
+++ api/routes/orders.js
@@ -2,11 +2,11 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/', (req, res, next) => {
-    res.status(200).json({ massage: 'Handling GET requests to /product'});
+    res.status(200).json({ massage: 'Handling GET order requests to /product'});
 });
 
 router.post('/', (req, res, next) => {
-    res.status(201).json({ massage: 'Handling Post product created to /product'});
+    res.status(201).json({ massage: 'Handling Post order created to /product'});
 });
 
 router.get('/:id', (req, res, next) => {
