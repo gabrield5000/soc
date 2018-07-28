0e24c9b02f9474e9ca22d2c305a8ae52c83198c6
gabrield5000 <gabrield5000@gmail.com>
2018-07-28 09:20:31 +0300

    add file upload

--- api/models/product.js
+++ api/models/product.js
@@ -4,7 +4,8 @@ const productSchema = mongoose.Schema({
     // _id: mongoose.Schema.Types.ObjectId,
     name: String,
     price: Number,
-    date: { type: Date, default: Date.now }
+    date: { type: Date, default: Date.now },
+    imageUrl: { type: String, require: true }
 });
 
 module.exports = mongoose.model('Product', productSchema);
--- api/routes/products.js
+++ api/routes/products.js
@@ -1,6 +1,33 @@
 const express = require('express');
 const router = express.Router();
 const mongoose = require('mongoose');
+const multer = require('multer');
+
+const storage = multer.diskStorage({
+    destination: function(req, file, cb) {
+        cb(null, './upload/');
+    },
+    filename: function (req, file, cb) {
+        cb(null, new Data().toISOString + file.originalname);
+    }
+});
+
+const fileFilter = (req, file, cb) => {
+    if(file.mimetype === 'image/jpeg' || file.mimetype === 'image/png') {
+        cb (null, true);
+    } else {
+        cb (null, false);
+    }
+}
+
+const upload = multer({ 
+    storage: storage, 
+    limits: {
+        fieldSize: 1024 * 1024 * 5
+    },
+    fileFilter: fileFilter
+});
+
 const Product = require('../models/product');
 
 router.get('/', (req, res, next) => {
@@ -14,7 +41,15 @@ router.get('/', (req, res, next) => {
                 request: {
                     type: 'GET'
                 },
-                products: docs
+                // products: docs
+                products : docs.map(doc => {
+                    return {
+                        name: doc.name,
+                        price: doc.price,
+                        imageUrl: doc.imageUrl,
+                        id: doc._id
+                    };
+                })
             }
             res.status(200).json(response);
         })
@@ -26,11 +61,12 @@ router.get('/', (req, res, next) => {
         });
 });
 
-router.post('/', (req, res, next) => {
+router.post('/', upload.single('Image'), (req, res, next) => {
     console.log(req.body);
     const product = new Product({
         name: req.body.name,
         price: req.body.price,
+        imageUrl: req.file.path
     });
 
     product
@@ -57,7 +93,7 @@ router.post('/', (req, res, next) => {
 router.get('/:id', (req, res, next) => {
     const id = req.params.id;
     Product.findById(id)
-        .select( 'name price _id' )
+        .select( 'name imageUrl price _id' )
         .exec()
         .then( doc => {
             console.log('get one entry from the DB', doc);
